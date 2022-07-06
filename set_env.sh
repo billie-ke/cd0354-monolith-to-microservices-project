bc451e3c193850b241ea6ac8c93dc993038b316e
billie-ke <kirukuwambui@gmail.com>
2022-07-06 22:51:25 +0900

    all in one conf

--- set_env.sh
+++ set_env.sh
@@ -9,3 +9,5 @@ export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+export DOCKER_USERNAME=simons22d
+export DOCKER_PASSWORD=Stainly22d
